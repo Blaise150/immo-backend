19e7f397e13cfba85f982027d153d551e11bdfc3
Blaise <wattadoo150@gmail.com>
2026-02-16 18:02:40 +0100

    Fix DATABASE_URL configuration

--- immo_api/settings.py
+++ immo_api/settings.py
@@ -78,7 +78,7 @@ DATABASE_URL = config('DATABASE_URL', default='')
 if DATABASE_URL:
     DATABASES = {
         'default': dj_database_url.config(
-            default=DATABASE_URL,
+            default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
             conn_max_age=600
         )
     }
